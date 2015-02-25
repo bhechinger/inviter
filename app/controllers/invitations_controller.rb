c991fff65fbaa42efbde26c6693e85708abbf25c
Brian Hechinger <bhechinger@sjm.com>
2015-02-25 11:03:53 -0500

    bah, it should be USERNAME not USER

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -17,10 +17,10 @@ class InvitationsController < ApplicationController
   end
 
   def authenticate
-    return true if (ENV['HTTP_BASIC_USER'].nil? || ENV['HTTP_BASIC_PASSWORD'].nil?)
+    return true if (ENV['HTTP_BASIC_USERNAME'].nil? || ENV['HTTP_BASIC_PASSWORD'].nil?)
 
     authenticate_or_request_with_http_basic do |user, password|
-      user == ENV['HTTP_BASIC_USER'] && password == ENV['HTTP_BASIC_PASSWORD']
+      user == ENV['HTTP_BASIC_USERNAME'] && password == ENV['HTTP_BASIC_PASSWORD']
     end
   end
 end
